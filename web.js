a004cb4ca596b954e7cc8a329956a7e6811a17ab
SuFizz <ee10b041@ee.iitm.ac.in>
2013-07-08 00:59:42 +0900

    Update web.js

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs')
 
 var app = express.createServer(express.logger());
-
+var buffer = new Buffer(256);
+buffer = fs.readFileSync('index.html');
+//console.log(free);
+//buffer.write(free,'');
+var outp = buffer.toString('utf-8',0,100)
+console.log(outp)
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(outp);
 });
 
 var port = process.env.PORT || 5000;
